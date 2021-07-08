d94e056f62e35a51031aea076b59831143ec9e5f
Momen-Code <59899828+Momen-Code@users.noreply.github.com>
2021-07-08 16:02:34 -0700

    added the home page
    but there is some more tasks to di in it not the final phase

---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Employees } from "./Employees/Employees";
+export { default as Services } from "./Services/Services";
+export { default as Box } from "./FloatingBox/FloatingBox";
