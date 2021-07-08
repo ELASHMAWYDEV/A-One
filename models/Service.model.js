2e8ff16b26a2837fec0fca6a73b39277ac1064bc
ELASHMAWYDEV <elashmawydev@gmail.com>
2021-07-08 23:42:36 +0200

    finished all DB models

---
+++ models/Service.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+
+const ServiceSchema = new mongoose.Schema({
+	name: String,
+	price: Number,
+});
+
+
+module.exports = mongoose.model("Service", ServiceSchema, "services");
