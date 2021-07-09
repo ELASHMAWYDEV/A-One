c6ac94020d76cb3d1bb5c56c3ad859a6ce27d51a
ELASHMAWYDEV <elashmawydev@gmail.com>
2021-07-09 20:27:58 +0200

    Added init file

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require("path");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
+//Init
+require("./init");
+
 //Middlewares
 app.use(cors());
 app.use(express.json());
@@ -11,13 +14,12 @@ app.use(express.json());
 //API
 app.use("/api", require("./routes"));
 
-
 //For the react app
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+	app.use(express.static(path.join(__dirname, "client", "build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
 }
 /*********************************************************/
 
